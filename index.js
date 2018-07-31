48517a0c1e025ce62d79f10bc00588d115968b7a
Alex <alojoshka@gmail.com>
2018-07-31 22:16:18 +0300

    SSR STEP 1

--- index.js
+++ index.js
@@ -2,15 +2,30 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express();
+const pageUrl = 'https://alpha.mouzenidis-travel.ru/reactssr/hottourspage';
+const pageUrl1 = 'https://alpha.mouzenidis-travel.ru/home/index3';
+
+async function ssr(url) {
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+  await page.goto(url, {waitUntil: 'networkidle0'});
+  const html = await page.content(); // serialized HTML of page DOM.
+  await browser.close();
+  return html;
+}
+
 
 const rend=async () => {
   console.log("async function called");
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage()
   //await page.setViewport({ width: 1280, height: 800 })
-  await page.goto('https://alpha.mouzenidis-travel.ru/reactssr/hottourspage', {waitUntil: 'networkidle0'})
+
+
+
+  await page.goto(pageUrl1, {waitUntil: 'networkidle0'})
   const name = await page.content();//await page.evaluate(() => page.content())
-  console.log(name);
+ // console.log(name);
   
   // const name = await page.evaluate(() => document.querySelector('.hot-tours-react').innerText)
   // console.log(name)
@@ -30,7 +45,7 @@ const rend=async () => {
 app.get('/', function (req, res) {
     console.log("route index started");
    
-     rend().then(html=>res.send(html)).catch(console.error);
+     ssr(pageUrl1).then(html=>res.send(html)).catch(console.error);
      //res.send('Hello World 113!');
   });
   const port = process.env.PORT || 8080;
